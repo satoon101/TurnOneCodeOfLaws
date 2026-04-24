028f1be0b3e3724be32f4a0707c8f627336fc331
satoon101 <satoon101@gmail.com>
2026-04-23 23:22:34 -0400

    Fixed bug that caused civic to be activated in games starting in a later era than ancient.

--- scripts/Gameplay/TurnOneCodeOfLaws.lua
+++ scripts/Gameplay/TurnOneCodeOfLaws.lua
@@ -6,15 +6,20 @@
 print("=== Turn One Code of Laws (Gameplay) Loading ===")
 
 function OnPlayerTurnActivated(playerID)
-    local player = Players[playerID];
+    local currentTurn = Game.GetCurrentGameTurn()
+    if currentTurn ~= 1 then
+        return
+    end
+
+    local player = Players[playerID]
     if not player:IsHuman() then
         return
     end
 
-    local culture = player:GetCulture();
-    local civicIndex = GameInfo.Civics["CIVIC_CODE_OF_LAWS"].Index;
-    local cost = culture:GetCultureCost(civicIndex);
-    culture:SetCulturalProgress(civicIndex, cost);
+    local culture = player:GetCulture()
+    local civicIndex = GameInfo.Civics["CIVIC_CODE_OF_LAWS"].Index
+    local cost = culture:GetCultureCost(civicIndex)
+    culture:SetCulturalProgress(civicIndex, cost)
 end
 
 Events.PlayerTurnActivated.Add(OnPlayerTurnActivated)
